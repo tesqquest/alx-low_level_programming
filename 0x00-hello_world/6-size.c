057426872e93600f5c8551a7f20e6c827e46e89e
tesqquest <tesqquest@gmail.com>
2022-07-01 00:35:27 +0100

    Sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long int c=1;
 	long long int d=1;
 	float e=10.1;
-	printf("Size of a char: %c bytes", sizeof(a));
-	printf("Size of an int: %d bytes", sizeof(b));
-	printf("Size of a long int: %d bytes", sizeof(c));
-	printf("Size of a long long int: %d bytes", sizeof(d));
-	printf("size of a float: %f bytes", sizeof(e));
+	printf("Size of a char: %d byte\(s\)\n", sizeof(a));
+	printf("Size of an int: %d byte\(s\)\n", sizeof(b));
+	printf("Size of a long int: %d byte\(s\)\n", sizeof(c));
+	printf("Size of a long long int: %d byte\(s\)\n", sizeof(d));
+	printf("size of a float: %d byte\(s\)\n", sizeof(e));
 	return (0);
 }
