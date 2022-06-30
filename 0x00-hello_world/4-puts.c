eeaabf3c0859fd6757b60a00b69e67f6edbf480c
tesqquest <tesqquest@gmail.com>
2022-06-30 23:17:05 +0100

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+/*We still breathing*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
