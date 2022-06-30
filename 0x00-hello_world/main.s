779eabcc3c03a7d7d37d3d8b43aa9c66cc0d09f2
tesqquest <tesqquest@gmail.com>
2022-07-01 00:50:33 +0100

    Intel

---
+++ 0x00-hello_world/main.s
@@ -0,0 +1,20 @@
+	.file	"main.c"
+	.intel_syntax noprefix
+# GNU C17 (GCC) version 11.3.1 20220421 (Red Hat 11.3.1-2) (x86_64-redhat-linux)
+#	compiled by GNU C version 11.3.1 20220421 (Red Hat 11.3.1-2), GMP version 6.2.0, MPFR version 4.1.0-p13, MPC version 1.2.1, isl version none
+# GGC heuristics: --param ggc-min-expand=100 --param ggc-min-heapsize=131072
+# options passed: -masm=intel -mtune=generic -march=x86-64 -Og
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB11:
+	.cfi_startproc
+# main.c:11: }
+	mov	eax, 0	#,
+	ret	
+	.cfi_endproc
+.LFE11:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 11.3.1 20220421 (Red Hat 11.3.1-2)"
+	.section	.note.GNU-stack,"",@progbits
