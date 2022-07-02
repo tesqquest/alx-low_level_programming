d22fe6b7c6506b5fd8fae5ea48f2d04fa6481789
tesqquest <tesqquest@gmail.com>
2022-07-02 01:56:07 +0100

    Last

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,13 +14,14 @@
 
 int main(void)
 {
-	int n,last;
+	int n, last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last = n % 10;
+
 	/* your code goes there */
-	
+
 	printf("Last digit of %d ", n);
 
 	if (last > 5)
