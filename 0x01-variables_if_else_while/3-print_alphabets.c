20d0517baeaf91b3028c50833254f3c2b08412d7
tesqquest <tesqquest@gmail.com>
2022-07-02 02:26:56 +0100

    Print

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char n;
+
+	n = 'a';
+	x = 'A'
+
+	while (n <= 'z' && x <= 'Z')
+	{
+		putchar(n);
+		putchar(x);
+		n++;
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
