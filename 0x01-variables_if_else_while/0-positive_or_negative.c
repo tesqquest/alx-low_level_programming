011280161f061abd0c0aa333a2c303f34ff3add8
tesqquest <tesqquest@gmail.com>
2022-07-02 01:34:15 +0100

    PoN

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,6 +5,12 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
+ * */
 
 int main(void)
 {
