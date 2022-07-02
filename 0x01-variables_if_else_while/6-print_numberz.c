0e71ebac0d34e1e2e2d98a3056130fe135f0e886
tesqquest <tesqquest@gmail.com>
2022-07-02 03:01:46 +0100

    Numbaz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n <= 9)
+	{
+		putchar(n);
+		n++;
+	}
+
+	putchar('\n');
+	return (0);
+}
