ebb0192be3df6a55ab53f7e9cd7dd072139ca8c1
tesqquest <tesqquest@gmail.com>
2022-07-05 22:06:10 +0100

    New age

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - check the code
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Always 0.
  **/
-int _putchar(char c)
+
+int main(void)
 {
-	return (write(1, &c, 1));
+	char test[9] = "_putchar";
+	int i;
+
+	for (i = 0 ; i <= 10 ; i++)
+	{
+		_putchar(test[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
 }
