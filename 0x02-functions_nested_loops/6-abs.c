1a22136edcd9d24e763ddfc7529f5edf53ad7f34
tesqquest <60857000+tesqquest@users.noreply.github.com>
2022-07-06 03:06:55 +0100

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include  "main.h"
+
+
+/**
+ * _abs - print_alphabet
+ * @c: its mine damnit
+ *
+ * Return: 1 if lowercase and 0 otherwise;
+ * Does my stuff explained.
+ */
+
+int _abs(int c)
+{
+	if (c > 0)
+	{
+		return (c);
+	}
+	return (-c);
+}
